e9869e35c93c4c60c58eb9ec5110be0bb7a5ae4b
CurryQuantum <pedregosa.ernest@gmail.com>
2025-11-13 02:55:21 +0000

    final version below:

--- ParenthesesChecker.java
+++ ParenthesesChecker.java
@@ -1,44 +1,45 @@
 public class ParenthesesChecker {
-
+     //  Class author:  ernest pedregosa delserieys
+     //  Date created:  11/12/2025
+     //  General description: checks if a string of parentheses is valid or not
+     // Precondition: S is a string containing only the characters '(', ')', '{', '}', '[' and ']'.
+     // Postcondition: Prints "Valid" if the parentheses in the string are properly matched and nested ; otherwise, prints "Invalid".
+    
 
     public static void isValid(String S){
-        //s.indexOf(String something)
-        //s.sutbstring(index1, index2) 
-        
         //find a pair of "complete" characters so either (), [] or {}
         //if you do, get rid of them and do it again. 
         // if you don't if the string is empty it's valid if not empty not valid
 
+        int length = S.length();
 
-       char i;
-       for (int x = 0; x <= S.length(); x++) {
-        if(S.substring(x) == '('){}
-       }  
-        while(S.length() != 0){
-            
+        while (length > 0) {
+        // for (int j = 0; j < 10; j++) {
+            // The replace() method removes ALL instances of the pair.
+            // We use replace() here because it's a simple, single command.
+            S = S.replace("()", "");
+            S = S.replace("{}", "");
+            S = S.replace("[]", "");
 
-            //start by finding the LAST ( or [ or {
-            if(S.charAt(i)== '('){
-            
+            // If no pairs were removed, break out of the loop.
+            if (length == S.length()) {
+                break;  
             }
-
-
-        if ((S.indexOf("(") - S.indexOf(")")) == 1){
-
-        
-
-         S = S.substring ( S.indexOf ( "(" ) , S.indexOf (")") + 1);
-        }
-        else if ((S.indexOf("{") - S.indexOf("}")) == 1){
-         S -= S.substring ( S.indexOf ( "{" ) , S.indexOf ("}") + 1);
-        }
-        else if ((S.indexOf("[") - S.indexOf("]")) == 1){
-         S -= S.substring ( S.indexOf ( "[" ) , S.indexOf ("]") + 1);
+            else{
+                // Keep track of the current length before we try to remove pairs.
+                length = S.length();
+            }
         }
 
-        } 
+        if (S.length() == 0) {
+            System.out.println("Valid");
+        } else {
+            System.out.println("Invalid");
+        }    
+    }
+    
+    public static void main(String[] args){
+        isValid("{{()}}");
+    }
 
- 
-
- }
 }
